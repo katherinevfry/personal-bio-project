e05c98a7f9cef3d14a80ca8f8271041ff1026ee7
Katy-fry <katyvfry@gmail.com>
2021-03-13 15:21:36 -0600

    added irebase and small layout tweaks

--- src/javascripts/components/projectBuilder.js
+++ src/javascripts/components/projectBuilder.js
@@ -1,18 +1,22 @@
 const createProjectCards = (taco) => {
   let domString = '';
-  taco.forEach((element, i) => {
-    if (element.available === true) {
-      domString += `<div class="card text-center" style="width: 30rem;" id= ${i}>
-        <img src="${element.screenshot}" class="card-img-top" alt="project screenshot">
+  taco.forEach((element) => {
+    domString += `<div class="card mb-3" style="width: 60rem">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img class="card-img" src="${element.screenshot}" alt="project screenshot">
+      </div>
+      <div class="col-md-8">
         <div class="card-body">
-          <h3 class="card-title">${element.title}</h3>
+          <h5 class="card-title">${element.title}</h5>
           <p class="card-text description"> ${element.description}</p>
-          <p class="card-text techs"> ${element.technologiesUsed}</p>
-          <a href="${element.url}" class="btn btn-outline-light">Project</a>
-          <a href="${element.githubUrl}" class="btn btn-outline-light">GitHub</a>
+            <p class="card-text techs"> ${element.technologiesUsed}</p>
+            <a href="${element.url}" class="btn btn-outline-light">Project</a>
+            <a href="${element.githubUrl}" class="btn btn-outline-light">GitHub</a>
         </div>
-      </div>`;
-    }
+      </div>
+    </div>
+  </div>`;
   });
   document.querySelector('#projectsDisplay').innerHTML = domString;
 };
